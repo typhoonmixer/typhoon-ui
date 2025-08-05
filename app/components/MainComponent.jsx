252f21f03ca4d17ed0107e2737a6ec26718cd626
Typhoon <typhooncoinmixer@gmail.com>
2025-08-05 19:54:20 -0300

    Update MainComponent.jsx

--- app/components/MainComponent.jsx
+++ app/components/MainComponent.jsx
@@ -218,7 +218,7 @@ const MainComponent = () => {
     if (typeof window !== 'undefined') {
       localStorage.setItem("curToken", "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d");
     }
-    const noteAccount = "";
+    let noteAccount = "";
     if (typeof window !== "undefined") {
       noteAccount = localStorage.getItem("noteAcc") != null ? localStorage.getItem("noteAcc") : "";
     }
