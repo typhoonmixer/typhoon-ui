ba1a75558d41ac20311e100cb22d967a968e9b61
Typhoon <typhooncoinmixer@gmail.com>
2025-08-05 21:37:29 -0300

    Update NoteList.jsx

--- app/components/NoteList.jsx
+++ app/components/NoteList.jsx
@@ -95,10 +95,7 @@ function NoteList() {
         if (typeof window !== "undefined") {
             noteAccount = localStorage.getItem('noteAcc');
         }
-        console.log("note account before ", noteAccount)
         if (noteAccount != null && noteAccount != "undefined" && noteAccount != "null") {
-            console.log("inside fetch notes ", noteAccount)
-            
             fetchNotes();
         }
 
