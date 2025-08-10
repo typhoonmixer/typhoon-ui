008fe96174710c85c2f4367043c4de34c83a5588
Typhoon <typhooncoinmixer@gmail.com>
2025-08-10 12:27:07 -0300

    Update SupportedDenominations.js

--- app/utils/SupportedDenominations.js
+++ app/utils/SupportedDenominations.js
@@ -12,7 +12,8 @@ export const denominationsList = {
      "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7": ['0.01', '0.1', '1', '10'],
      "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8": ['10', '100', '1000', '10000'],
      "0x0719b5092403233201aa822ce928bd4b551d0cdb071a724edd7dc5e5f57b7f34": ['10', '100', '1000', '10000'],
-     "0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac": ['0.001', '0.01', '0.1', '1']
+     "0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac": ['0.001', '0.01', '0.1', '1'],
+     "0x04daa17763b286d1e59b97c283c0b8c949994c361e426a28f743c67bdfe9a32f": ['0.001', '0.01', '0.1', '1'],
 }
 
 export const tokenList = {
@@ -20,7 +21,8 @@ export const tokenList = {
      "ETH": "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
      "USDC": "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
      "UNO": "0x0719b5092403233201aa822ce928bd4b551d0cdb071a724edd7dc5e5f57b7f34",
-     "WBTC": "0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac"
+     "WBTC": "0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac",
+     "tBTC": "0x04daa17763b286d1e59b97c283c0b8c949994c361e426a28f743c67bdfe9a32f"
 }
 
 export const tokenDecimals = {
@@ -28,5 +30,6 @@ export const tokenDecimals = {
      "ETH": 18,
      "USDC": 6,
      "UNO": 18,
-     "WBTC": 8
+     "WBTC": 8,
+     "tBTC": 18
 }
