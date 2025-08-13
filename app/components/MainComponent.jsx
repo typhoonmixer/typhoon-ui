079952cc0064139f178d591c70ce970f4ab079ca
Typhoon <typhooncoinmixer@gmail.com>
2025-08-13 13:22:50 -0300

    Update MainComponent.jsx

--- app/components/MainComponent.jsx
+++ app/components/MainComponent.jsx
@@ -759,7 +759,21 @@ const MainComponent = () => {
       createAndDownloadFile(proofElements)
     } else {
       setLoadingText("Saving in Note Account!(Do not close neither reload the screen.)")
-      await saveInNoteAccount(["0x" + secret, "0x" + nullifier, multiCall.transaction_hash.toString(), poolAddr, rewardMode ? "0x" + day.toString() : '0x1'])
+      
+      try{
+        await saveInNoteAccount(["0x" + secret, "0x" + nullifier, multiCall.transaction_hash.toString(), poolAddr, rewardMode ? "0x" + day.toString() : '0x1'], noteAcc)
+      } catch (e) {
+        console.log(e)
+        let proofElements = JSON.stringify({
+          "secret": "0x" + secret,
+          "nullifier": "0x" + nullifier,
+          "txHash": multiCall.transaction_hash.toString(),
+          "pool": poolAddr,
+          "day": rewardMode ? "0x" + day.toString() : '0x1'
+        })
+        createAndDownloadFile(proofElements)
+      }
+      
     }
 
     await new Promise(r => setTimeout(r, 2000));
