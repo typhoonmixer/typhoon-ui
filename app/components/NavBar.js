f6b546375d7d8129c78fa2df7e406118418b81de
typhoonmixer <typhooncoinmixer@gmail.com>
2025-08-31 11:47:30 -0300

    fixed navbar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
         <nav className="p-4 bg-transparent text-white flex items-center " style={{ zIndex: 500, backgroundColor: 'transparent', position: 'relative', pointerEvents: 'auto' }}>
             <Link href="/" className="mr-4 flex items-center">
                 <img src="/Typhoon_logo.png" alt="Typhoon Logo" className="h-10 w-auto mr-2" />
-                <span className="text-white text-3xl font-bold">Typhoon</span>
+                <span className="text-white text-2xl font-bold">Typhoon</span>
             </Link>
             <Link href="/compliance" className="ml-4"><span className={` text-base font-semibold ${pathname === '/compliance' ? 'text-blue-500' : 'text-white'} hover:text-blue-500`}>Compliance</span></Link>
             <a target="_blank" rel="noopener noreferrer" href='https://typhoon-2.gitbook.io/typhoon-docs' className="ml-4"><span className='text-base font-semibold text-white hover:text-blue-500'>Docs</span></a>
